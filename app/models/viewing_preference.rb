d4c8dfdee6df3bb718f674c0c0bd7963d8f4725a
dylanpavelko <dylan@dylanpavelko.com>
2015-08-09 16:13:14 -0700

    adjusted date ranges for pre-1989 years

--- app/models/viewing_preference.rb
+++ app/models/viewing_preference.rb
@@ -3,11 +3,34 @@ class ViewingPreference < ActiveRecord::Base
 	def includes(group)
 		if self.startDate == Date.new(1984,1,1)
 			start=Date.new(1000,1,1)
+		elsif self.startDate == Date.new(1985,1,1)
+			start=Date.new(1800,1,1)
+		elsif self.startDate == Date.new(1986,1,1)
+			start=Date.new(1911,1,1)	
+		elsif self.startDate == Date.new(1987,1,1)
+			start=Date.new(1958,1,1)
+		elsif self.startDate == Date.new(1988,1,1)
+			start=Date.new(1988,1,1)
 		else
 			start=self.startDate
 		end
+
+		if self.endDate == Date.new(1984,1,1)
+			endDateRange=Date.new(1000,1,1)
+		elsif self.endDate == Date.new(1985,1,1)
+			endDateRange=Date.new(1800,1,1)
+		elsif self.endDate == Date.new(1986,1,1)
+			endDateRange=Date.new(1911,1,1)	
+		elsif self.endDate == Date.new(1987,1,1)
+			endDateRange=Date.new(1958,1,1)
+		elsif self.endDate == Date.new(1988,1,1)
+			endDateRange=Date.new(1988,10,1)
+		else
+			endDateRange=self.endDate
+		end
+
 		@included = false
-		if group.isIncludedInDateRange(start, self.endDate)
+		if group.isIncludedInDateRange(start, endDateRange)
 			@included = true
 		end
 		return @included
